f9d9676c10295cef6e133b97e59c1884208f1ff5
Alireza Kasir <alirezakasir@yahoo.com>
2022-02-19 23:34:23 +0330

    edit active class for dropdown with hover

--- js/main.js
+++ js/main.js
@@ -1,15 +1,50 @@
 // header dropdown manage style when open
-let myDropdownsHeader = document.querySelectorAll(
+let myDropdownsTogglerHeader = document.querySelectorAll(
   ".main-header [data-bs-toggle='dropdown']"
 );
-myDropdownsHeader = Array.from(myDropdownsHeader);
+myDropdownsTogglerHeader = Array.from(myDropdownsTogglerHeader);
 
-myDropdownsHeader.forEach((dropdown) => {
-  dropdown.addEventListener("show.bs.dropdown", function () {
-    dropdown.classList.add("main-header__nav__row__right__link--active");
+myDropdownsTogglerHeader.forEach((dropdownToggler) => {
+  // init the dropdown
+  var dropdownInstance = new bootstrap.Dropdown(dropdownToggler, {
+    popperConfig: function (defaultBsPopperConfig) {
+      // var newPopperConfig = {...}
+      // use defaultBsPopperConfig if needed...
+      // return newPopperConfig
+    },
+  });
+  const dropdown = dropdownInstance._menu;
+  let cliced = false;
+
+  // mouse enter to dropdoen handel
+  dropdown.addEventListener("mouseenter", () => {
+    if (!cliced) {
+      dropdownToggler.classList.add(
+        "main-header__nav__row__right__link--active"
+      );
+    }
   });
 
-  dropdown.addEventListener("hide.bs.dropdown", function () {
-    dropdown.classList.remove("main-header__nav__row__right__link--active");
+  // mouse leave to dropdoen handel
+  dropdown.addEventListener("mouseleave", () => {
+    if (!cliced) {
+      dropdownToggler.classList.remove(
+        "main-header__nav__row__right__link--active"
+      );
+    }
+  });
+  // show dropdown handel
+  dropdownToggler.addEventListener("show.bs.dropdown", function () {
+    dropdownToggler.classList.add("main-header__nav__row__right__link--active");
+    cliced = true;
+  });
+
+  // hide dropdown handel
+
+  dropdownToggler.addEventListener("hide.bs.dropdown", function () {
+    dropdownToggler.classList.remove(
+      "main-header__nav__row__right__link--active"
+    );
+    cliced = false;
   });
 });
