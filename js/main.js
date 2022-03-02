264c05aae81e869c39424a057c00db546215536c
Alireza Kasir <alirezakasir@yahoo.com>
2022-03-02 18:19:46 +0330

    add style of slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 import Lightense from "./lightense.min";
 import Dropdown from "bootstrap/js/dist/dropdown";
 import Tooltip from "bootstrap/js/dist/tooltip";
+import { tns } from "tiny-slider";
 import "bootstrap/js/dist/offcanvas";
 import "bootstrap/js/dist/collapse";
 // initial
 window.Dropdown = Dropdown;
 window.Tooltip = Tooltip;
 window.Lightense = Lightense;
-
+window.tns = tns;
 import "./base";
